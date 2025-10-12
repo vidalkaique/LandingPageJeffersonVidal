b4145de73c7e027aca7e3afee571c76e2a5cd2bf
vidalkaique <dosreiskaiquevidal@gmail.com>
2025-10-12 18:34:21 -0300

    atualizando foto

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
               {/* Imagem com efeito de bordas transparentes suaves */}
               <div className="relative w-full h-full">
                 <img 
-                  src="/img/JEFFERSON_VIDAL.png" 
+                  src="/img/JEFFERSON_VIDAL_2.png" 
                   alt="Jefferson Vidal - Personal Trainer"
                   className="w-full h-full object-cover object-center"
                 />
